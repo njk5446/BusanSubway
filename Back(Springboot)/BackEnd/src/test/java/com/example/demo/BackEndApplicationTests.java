07315ddcc9c009e985d9c79fd5927adce1d7f056
njk5446 <njk5446@naver.com>
2024-11-20 15:55:43 +0900

    spring boot 빌드 테스트

--- Back(Springboot)/BackEnd/src/test/java/com/example/demo/BackEndApplicationTests.java
+++ Back(Springboot)/BackEnd/src/test/java/com/example/demo/BackEndApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.subway.BackEndApplication;
+
+@SpringBootTest(classes = BackEndApplication.class)
 class BackEndApplicationTests {
 
 	@Test
