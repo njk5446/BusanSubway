14e6f7af3b191ad720071f0071289831c28eb3bd
njk5446 <njk5446@naver.com>
2024-11-20 14:44:28 +0900

    oauth google url 추가

--- Back(Springboot)/BackEnd/src/main/java/com/subway/config/OAuth2SuccessHandler.java
+++ Back(Springboot)/BackEnd/src/main/java/com/subway/config/OAuth2SuccessHandler.java
@@ -3,6 +3,7 @@ package com.subway.config;
 import java.io.IOException;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -29,6 +30,9 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 	private final MemberRepository mr;
 	private final PasswordEncoder enc;
 	private final RandomNicknameService rns;
+	
+	@Value("${oauth.google.redirect.url}")
+	private String googleUrl;
 
 	// OAuth 로그인 성공 시 호출되는 메서드 
 	@Override
@@ -67,9 +71,9 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 		String jwtToken = JWTUtil.getJWT(userName).substring(7);
 		
 		// 배포용 연결
-		response.sendRedirect("http://58.235.21.221:4000/checkToken?token=" + jwtToken);
+//		response.sendRedirect("http://58.235.21.221:4000/checkToken?token=" + jwtToken);
 		// 로컬(개발용) 연결 
-//		response.sendRedirect("http://192.168.55.203:4000/checkToken?token=" + jwtToken);
+		response.sendRedirect(googleUrl + "/checkToken?token=" + jwtToken);
 		// OAuth 로그인 성공 시, 클라이언트를 이 토큰을 포함한 주소로 리다이렉트(이 url을 브라우저에서 위치 시킴)
 	}
 }
